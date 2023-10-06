64097c328aa2473af5d45a8a4fa4c67a5e120d61
MohanManirul <fiforeg@gmail.com>
2023-10-06 16:51:29 +0600

    static function

--- oop_practice2/static/Attendee.php
+++ oop_practice2/static/Attendee.php
@@ -1,5 +1,18 @@
 <?php
 
-    
+    class Attendee{
+        private static int $count = 0 ;
+
+        public function __construct()
+        {
+            self::$count++ ;
+        }
+        public static function getCount(){
+            return self::$count;
+        }
+    }
+
+    // $mohan = new Attendee();
+    print_r(Attendee::getCount());
 
 ?>
