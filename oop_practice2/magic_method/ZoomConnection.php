eecc97f5c1027ba2b76fb545473b9a947ac2ff7d
MohanManirul <fiforeg@gmail.com>
2023-10-06 19:22:36 +0600

    get & set magic method

--- oop_practice2/magic_method/ZoomConnection.php
+++ oop_practice2/magic_method/ZoomConnection.php
@@ -3,6 +3,16 @@
 class ZoomConnection{
     private static int $count = 0;
 
+    private string $connectionID ;
+
+    public function __set($name , $value){
+        $this->$name = $value;
+    }
+
+    public function __get($name ){
+       return $this->$name;
+    }
+
     public function __construct()
     {
         self::$count++ ;
@@ -19,7 +29,11 @@ class ZoomConnection{
 }
 
 $connection1 = new ZoomConnection();
-$connection2 = new ZoomConnection();
-unset($connection2);
-print $connection1->getCount();
+print $connection1->connectionID = "123";
+// $connection2 = new ZoomConnection();
+// unset($connection2);
+// print $connection1->getCount();
+
+
+
 ?>
